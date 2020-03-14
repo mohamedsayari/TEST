e0894d5f3494f1c7c98643177d53150c6732eb5c
mohamedsayari <medsayri2@gmail.com>
2020-03-14 23:49:37 +0100

    first commit

---
+++ src/main/java/com/example/my/projet/MyProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.my.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjetApplication.class, args);
+	}
+
+}
